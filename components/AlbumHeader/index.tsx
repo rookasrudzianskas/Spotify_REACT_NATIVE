0f109440f1be7857aeda2318e49a3f5e136413aa
Rokas <rookas.rudzenskas@gmail.com>
2021-07-02 06:46:27 +0300

    Rokas

--- components/AlbumHeader/index.tsx
+++ components/AlbumHeader/index.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import {View, Text} from "react-native";
+import {View, Text, Image, TouchableOpacity} from "react-native";
 import {Album} from "../../types";
+import styles from "./styles";
 
 export type AlbumHeaderProps = {
     album: Album,
 }
 
 const AlbumHeader = (props: AlbumHeaderProps) => {
-    return (
-        <View>
-            <View>
+    const {album} = props;
 
+    return (
+            <View style={styles.container}>
+                <Image source={{uri: album.imageUri}} style={styles.image} />
+                <Text style={styles.name}>{album.name}</Text>
+
+                <View style={styles.creatorContainer}>
+                    <Text style={styles.creator}>By {album.by}</Text>
+                    <Text style={styles.likes}>{album.numberOfLikes} Likes</Text>
+                </View>
+                <TouchableOpacity activeOpacity={0.8}>
+                    <View style={styles.button}>
+                        <Text style={styles.buttonText}>PLAY</Text>
+                    </View>
+                </TouchableOpacity>
             </View>
-        </View>
     );
 };
 
---
+++ components/AlbumHeader/styles.ts
@@ -0,0 +1,49 @@
+import {StyleSheet} from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        padding: 20,
+    },
+    image: {
+      width: 200,
+      height: 200,
+        margin: 15,
+    },
+    name: {
+        color: 'white',
+        fontSize: 30,
+        fontWeight: 'bold',
+    },
+    creatorContainer: {
+      flexDirection: "row",
+        margin: 10,
+    },
+    creator: {
+        color: "lightgrey",
+        margin: 5,
+        fontSize: 16,
+        textTransform: "uppercase",
+    },
+    likes: {
+        color: "lightgrey",
+        margin: 5,
+        fontSize: 16,
+        textTransform: "uppercase",
+    },
+    button: {
+        backgroundColor: "#1cd05d",
+        height: 50,
+        width: 175,
+        borderRadius: 50,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    buttonText: {
+      color: 'lightgrey',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+});
+
+export default styles;
